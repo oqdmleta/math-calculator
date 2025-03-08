d9e60bf4ba93ee3b13bf22f09ccebbf32252973a
oqdmleta <oqdmleta@gmx.com>
2025-03-08 15:00:49 +0000

    Add 5.py

---
+++ src/5.py
@@ -0,0 +1,11 @@
+def calculate(num1, num2, op):
+    if op == "+":
+        return num1 + num2
+    elif op == "-":
+        return num1 - num2
+    elif op == "*":
+        return num1 * num2
+    elif op == "/":
+        return num1 / num2
+    else:
+        raise ValueError("Invalid operator")
